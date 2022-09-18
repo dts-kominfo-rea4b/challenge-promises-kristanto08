836cf7648117d10f82008670c3f6ce089ccc312c
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-09-18 20:27:34 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,8 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
+
 const promiseOutput = null;
 
+// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
+const simplePromise = new Promise((resolve, reject) => {
+  // jika kondisi promiseOutput null maka
+  if (promiseOutput) {
+    const successMassage = 'berhasil';
+    resolve(successMassage);
+  } else {
+    const failMassage = 'gagal';
+    reject(failMassage);
+  }
+});
+
+simplePromise
+  .then((massage) => console.log(massage))
+  .catch((error) => console.log(error));
+
+
 module.exports = {
-  promiseOutput,
+promiseOutput,
 };
+
