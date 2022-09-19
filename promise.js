6ee392edf447a8ce3e97ecadcc9aeda2ae151087
kristanto08 <113617653+kristanto08@users.noreply.github.com>
2022-09-19 20:24:02 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,26 +1,26 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-
-const emosi = null;
-
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = new Promise((resolve, reject) => {
-  // jika kondisi promiseOutput null maka
-  if (emosi) {
-    const successMassage = 'berhasil';
-    resolve(successMassage);
-  } else {
-    const failMassage = 'gagal';
-    reject(failMassage);
+let filmMarah = 0;
+let filmTidakMarah = 0;
+const promiseOutput = (emosi) => {
+  if (emosi === 'marah') {
+    return promiseTheaterIXX().then((value) => {
+      value.forEach((array) => {
+        if (hasil === 'marah') {
+          filmMarah++;
+        } else if (array.hasil === 'tidak marah') {
+          filmTidakMarah++;
+        } else {
+          ('error');
+        }
+      });
+      //console.log(`hasil console: ${filmMarah}`);
+      return filmMarah;
+    });
   }
-});
-
-promiseOutput
-  .then((massage) => console.log(massage))
-  .catch((error) => console.log(error));
-
+};
 
 module.exports = {
-promiseOutput,
+  promiseOutput,
 };
+
